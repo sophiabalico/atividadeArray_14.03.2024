3d5eceac0915161702ff7ad1051a44a31c82a668
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-14 11:35:59 -0300

    segundo exemplo .reverse()

--- atividadearray.js
+++ atividadearray.js
@@ -10,7 +10,12 @@ console.log(numeros);
 //Saida esperada:
 //[ '5', '4', '3', '2', '1' ]
 
+
+// segundo exemplo
 let animais = ["elefante", "gato", "tigre", "leopardo", "vaca"]
 console.log(animais)
 animais.reverse()
 console.log(animais)
+
+//saida esperada:
+//[ 'vaca', 'leopardo', 'tigre', 'gato', 'elefante' ]
