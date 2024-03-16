612c5d487e4c3260d6a3f5762db302d51ba512c2
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-16 17:25:54 -0300

    método array .includes()

---
+++ arrayincludes.js
@@ -0,0 +1,19 @@
+// método que verifica se o elemento existe no array .includes()
+// vai criar uma variavel de cores
+let cor = ["roxo", "azul", "preto", "verde", "branco"]
+// vai criar outa variavel para verificar se o roxo existe no array
+let existe = cor.includes("roxo");
+// vai exibir se ele existe 
+console.log(existe);
+
+//Saida esperada:
+//existe
+
+
+// segundo exemplo
+let pais = ["brasil", "japao", "australia", "eua", "italia"]
+let possui = pais.includes("japao");
+console.log(possui);
+
+//Saida esperada:
+//possui
