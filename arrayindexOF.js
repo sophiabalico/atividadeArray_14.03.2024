1cb0523cf926ff9c7d74fe76e4d3c1609f146168
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 20:57:44 -0300

    método array indexOF

---
+++ arrayindexOF.js
@@ -0,0 +1,19 @@
+//método que retorna o primeiro index do elemento encontrado no array .indexOF()
+//vai criar uma variável de flores 
+let flores = ["rosa", "tulipa", "lirio", "girassol", "margarida"];
+//vai criar uma variável para mostra o index do elemento que você quer usando o método
+let index = flores.indexOf("lirio");
+//vai exibir o index do elemento que você colocou
+console.log(index);
+
+//saída esperada:
+//2
+
+
+//segundo exemplo
+let produtoshig = ["escova", "sabonete", "pasta", "shampoo", "esponja"];
+let indice = produtoshig.indexOf("esponja");
+console.log(indice);
+
+//saída esperada:
+//4
