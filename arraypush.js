795a337d31177438a43dfb74750791ce020352bd
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 14:42:25 -0300

    método array .push

---
+++ arraypush.js
@@ -0,0 +1,18 @@
+// método que inclui um novo elemento no final de um array .push()
+// vai criar uma variavel de frutas
+let frutas = ["banana", "morango", "pera", "manga", "uva"];
+// vai incluir esse novo elemento no array
+frutas.push("kiwi");
+// vai exibir o array com o novo elemento
+console.log(frutas);
+
+//Saida esperada:
+//[ 'banana', 'morango', 'pera', 'manga', 'uva', 'kiwi' ]
+
+// segundo exemplo
+let objeto = ["faca", "cadeira", "mesa", "armario", "colher"];
+objeto.push("garfo", "prato");
+console.log(objeto);
+
+//Saida esperada:
+//['faca', 'cadeira', 'mesa', 'armario', 'colher', 'garfo', 'prato']
