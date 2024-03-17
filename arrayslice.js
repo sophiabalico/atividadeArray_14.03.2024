e65c05518371cfe84a9b32854a7faed0c8f9702f
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 20:36:11 -0300

    método array .slice

---
+++ arrayslice.js
@@ -0,0 +1,23 @@
+// método que retorna uma copia do array .slice()
+// vai criar uma variavel de letras
+let arrayletras = ["a", "b", "c", "d", "e"];
+// vai criar outra variável para pegar o primeiro array e copiar ele começando a partir do índice 3 (d) 
+let novoarray = arrayletras.slice(3);
+// vai exibir o primeiro array
+console.log(arrayletras);
+// vai exibir a cópia do primeiro array começando a partir do índice 3
+console.log(novoarray);
+
+//Saida esperada:
+//[ 'a', 'b', 'c', 'd', 'e' ] primeiro array
+//[ 'd', 'e' ] segundo array
+
+// segundo exemplo
+let arraynumeros = ["1", "2", "3", "4", "5"];
+let copiaarray = arraynumeros.slice(2);
+console.log(arraynumeros);
+console.log(copiaarray);
+
+//Saida esperada:
+//[ '1', '2', '3', '4', '5' ]
+//[ '3', '4', '5' ]
