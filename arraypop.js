8d479d55b4eb82ac6249aaf1501a6d82877d7b75
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 16:37:13 -0300

    método array .pop

---
+++ arraypop.js
@@ -0,0 +1,18 @@
+// método que remove o último elemento de um array .pop()
+// vai criar uma variavel de marcas
+let marcas = ["Nike", "Adidas", "Gucci", "Prada", "Dior"];
+// vai remover o último elemento do array
+marcas.pop(marcas);
+// vai exibir o array com o último elemento removido
+console.log(marcas);
+
+//Saida esperada:
+//[ 'Nike', 'Adidas', 'Gucci', 'Prada' ]
+
+// segundo exemplo
+let redes = ["Instagram", "Facebook", "Whatsapp", "Spotify", "Gmail"];
+redes.pop(redes);
+console.log(redes);
+
+//Saida esperada:
+//[ 'Instagram', 'Facebook', 'Whatsapp', 'Spotify' ]
