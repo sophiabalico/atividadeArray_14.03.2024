5921468d8c1a968ebcd4e84c42dbf6baccad4cd0
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 20:02:38 -0300

    método array .join

---
+++ arrayjoin.js
@@ -0,0 +1,16 @@
+// método que cria uma string a partir dos elementos de um array .join()
+// vai criar uma variavel com palavras
+let frase = ["me", "chamo", "Sophia"];
+// vai pegar as palavras do array e criar uma string
+console.log(frase.join(" "));
+
+//Saida esperada:
+//me chamo Sophia
+
+
+// segundo exemplo
+let mensagem = ["eu", "gosto", "de", "tocar", "guitarra"];
+console.log(mensagem.join(" "));
+
+//Saida esperada:
+//eu gosto de tocar guitarra
