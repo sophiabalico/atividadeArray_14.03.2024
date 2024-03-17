e33899ad18173a18705e416d41b8ce21727bc09f
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 18:13:15 -0300

    método array .shift

---
+++ arrayshift.js
@@ -0,0 +1,18 @@
+// método que remove o primeiro elemento de um array .shift()
+// vai criar uma variavel de matérias
+let materias = ["portugues", "matematica", "biologia", "fisica"];
+// vai pegar o array e remover o primeiro elemento 
+materias.shift();
+// vai exibir o array com o primeiro elemento removido
+console.log(materias);
+
+//Saida esperada:
+//[ 'matematica', 'biologia', 'fisica' ]
+
+// segundo exemplo
+let esportes = ["handebol", "volei", "futebol", "basquete"];
+esportes.shift();
+console.log(esportes);
+
+//Saida esperada:
+//[ 'volei', 'futebol', 'basquete' ]
