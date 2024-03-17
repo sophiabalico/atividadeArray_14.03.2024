8980a3db777226cfe61d77385878f281ce3b47a5
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 14:08:29 -0300

    ajuste na saida esperada

--- arrayconcat.js
+++ arrayconcat.js
@@ -1,24 +1,24 @@
 // método que junta dois ou mais arrays .concat()
 // vai criar uma variavel de nomes
-let nome = ["Jesus", "Sophia", "Leonardo", "Eliane", "Jefferson", "Vitor"];
+let nome = ["Sophia", "Eliane", "Jefferson", "Vitor"];
 // vai criar outa variavel com outos nome para juntar com o outro array
-let novonome = nome.concat(["Bruna", "Brian"]);
+let novonome = nome.concat(["Renata", "Valeria"]);
 // vai exibir os nomes do primeiro array
 console.log(nome);
 // vai exibir o primeiro array junto com o novo array
 console.log(novonome);
 
 //Saida esperada:
-//"Jesus", "Sophia", "Leonardo", "Eliane", "Jefferson", "Vitor"
-//"Jesus", "Sophia", "Leonardo", "Eliane", "Jefferson", "Vitor", "Bruna", "Brian"
+//[ 'Sophia', 'Eliane', 'Jefferson', 'Vitor' ]
+//[ 'Sophia', 'Eliane', 'Jefferson', 'Vitor', 'Renata', 'Valeria' ]
 
 
 // segundo exemplo
-let letras = ["a", "b", "c", "d", "e"];
-let novasletras = letras.concat(["f", "g", "h"]);
+let letras = ["a", "b", "c",];
+let novasletras = letras.concat(["d", "e", "f"]);
 console.log(letras);
 console.log(novasletras);
 
 //Saida esperada:
-//"a", "b", "c", "d", "e"
-//"a", "b", "c", "d", "e", "f", "g", "h"
+//[ 'a', 'b', 'c' ]
+//[ 'a', 'b', 'c', 'd', 'e', 'f' ]
