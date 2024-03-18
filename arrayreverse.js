69a206cbcec4ad3e0685bfc3527dc686aa76d3bb
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-03-17 22:01:56 -0300

    alterei o nome da pasta

---
+++ arrayreverse.js
@@ -0,0 +1,21 @@
+// método que inverte o array .reverse()
+let numeros = ["1", "2", "3", "4", "5"];
+// vai exibir os numeros na ordem
+console.log(numeros);
+// vai pegar os numeros e inverter a ordem
+numeros.reverse()
+// vai exibir os numeros invertidos
+console.log(numeros);
+
+//Saida esperada:
+//[ '5', '4', '3', '2', '1' ]
+
+
+// segundo exemplo
+let animais = ["elefante", "gato", "tigre", "leopardo", "vaca"];
+console.log(animais);
+animais.reverse()
+console.log(animais);
+
+//saida esperada:
+//[ 'vaca', 'leopardo', 'tigre', 'gato', 'elefante' ]
